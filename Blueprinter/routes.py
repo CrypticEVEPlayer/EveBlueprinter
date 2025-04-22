8713563a16b4fcb0a69a36e83d7e697deaeac3e1
CrypticEVEPlayer <gatherercryptic@gmail.com>
2025-04-21 21:57:36 -0400

    made the BPO page Dyanmic

--- Blueprinter/routes.py
+++ Blueprinter/routes.py
@@ -1,19 +1,22 @@
 from flask import render_template, url_for, flash, redirect, request
 from Blueprinter import app, db
 from Blueprinter.models import t_industryActivityMaterials, InvTypes
-import sqlite3
-import pandas as pd
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    #invtype = InvTypes.query.get_or_404(997)
-    typeids_to_filter = [997]
-    materials = db.session.query(t_industryActivityMaterials).filter_by(typeID = 997).all()
-    test = InvTypes.query.get(997)
-    print(test)
+    return render_template('home.html')
+
+@app.route("/bpo/<int:typeid>", methods=['GET', 'POST'])
+def bpo(typeid):
+    materials = db.session.query(t_industryActivityMaterials).filter_by(typeID = typeid).all()
     print(len(materials))
+    if len(materials) == 0:
+        flash('Blueprint not found', 'danger')
+        return render_template('home.html')
     items = []
+    blueprint = InvTypes.query.get(typeid)
     for material in materials:
         print(material)
         invtype = InvTypes.query.get(material.materialTypeID)
@@ -24,20 +27,4 @@ def home():
         print(item)
         items.append(item)
     
-    return render_template('home.html', items=items)
-
-
-@app.route("/test")
-def test():
-    #invtype = InvTypes.query.get_or_404(997)
-    cnx = sqlite3.connect('instance/sqlite-latest.sqlite')
-    materials = pd.read_sql_query('SELECT * from industryActivityMaterials WHERE typeID = 997 AND activityID = 1', cnx)
-    items = []
-    for index, row in materials.iterrows():
-        invtype = pd.read_sql_query(f"SELECT * from InvTypes WHERE typeID = {row['materialTypeID']}", cnx)
-        item = {'id': row['materialTypeID'],
-                'qty': row['quantity'],
-                'name': invtype.loc[0]['typeName']}
-        items.append(item)
-    
-    return render_template('home.html', items=items)
+    return render_template('home.html', items=items, name=blueprint.typeName)
